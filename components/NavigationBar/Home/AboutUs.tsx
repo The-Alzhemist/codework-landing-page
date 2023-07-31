e079256bcb267d2b304cb0f1437ac53246e659be
Aphiwit1 <aphiwit.boon@gmail.com>
2023-07-31 15:28:45 +0700

    update:format

--- components/NavigationBar/Home/AboutUs.tsx
+++ components/NavigationBar/Home/AboutUs.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
-    <section className='max-w-7xl mx-auto py-[30px] md:py-[50px] px-4 '>
-        <h2 className="text-left mb-5 text-3xl md:text-5xl font-extrabold uppercase">About Us</h2>
-        <div className="border-2 rounded-md  border-green-700 bg-green-400  font-semibold text-2xl p-[30px] md:p-[100px]  text-center"> 
-        We are thrilled to announce the launch of our brand-new software house, and we can't wait to partner with you on your next project! At our state-of-the-art development hub, we've assembled a team of highly skilled and experienced senior developers and designers who are eager to bring your ideas to life.
-        </div>
+    <section className="max-w-7xl mx-auto py-[30px] md:py-[50px] px-4 ">
+      <h2 className="text-left mb-5 text-3xl md:text-5xl font-extrabold uppercase">
+        About Us
+      </h2>
+      <div className="border-2 rounded-md  border-green-700 bg-green-400  font-semibold text-2xl p-[30px] md:p-[100px]  text-center">
+        We are thrilled to announce the launch of our brand-new software house,
+        and we can't wait to partner with you on your next project! At our
+        state-of-the-art development hub, we've assembled a team of highly
+        skilled and experienced senior developers and designers who are eager to
+        bring your ideas to life.
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
