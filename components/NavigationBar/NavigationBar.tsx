ce4f07326d608be613f2397f186c60c45a7c3254
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-03 09:34:58 +0700

    update: hero section add svg

--- components/NavigationBar/NavigationBar.tsx
+++ components/NavigationBar/NavigationBar.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 
 const NavigationBar = () => {
-    // max-w-[1440px]
   return (
     <nav className="grid grid-cols-[30%_40%_30%] px-4  max-w-7xl mx-auto h-16 items-center ">
       <div>CODEWORK</div>
 
       <ul className=" hidden md:grid grid-cols-4 justify-center gap-2 justify-items-center items-center text-sm text-slate-800 ">
         <li className="bg-codework">About us</li>
-        <li className="text-yellow">Our services</li>
-        <li>Our project</li>
+        <li className="text-yellow">services</li>
+        <li>project</li>
         <li>Members</li>
       </ul>
 
       <div className="hidden md:flex items-center justify-end">
-        <button className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900  rounded-md border-2 border-[#C8D94C] hover:bg-[#C8D94C]">Contact us</button>
+        <button className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900  rounded-md border-2 border-[#C8D94C] hover:bg-[#C8D94C]">
+          Contact us
+        </button>
       </div>
+
+   
     </nav>
   );
 };
