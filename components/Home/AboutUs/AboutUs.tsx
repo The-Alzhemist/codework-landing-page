d5b218c1bf1fd9cf85d1a8419eef6a0aef768bb9
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-04 13:56:48 +0700

    update: font for about-us and team-culture

--- components/Home/AboutUs/AboutUs.tsx
+++ components/Home/AboutUs/AboutUs.tsx
@@ -1,24 +1,18 @@
 import React from "react";
 import BackgroundGradientBlur from "../../backgroundGradientBlur/BackgroundGradientBlur";
+import { Prompt } from "@next/font/google";
+
+const fontPrompt = Prompt({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-prompt",
+});
+
 
 const AboutUs = () => {
   return (
     <>
-      {/* <section className="max-w-7xl mx-auto py-[30px] md:py-[50px] px-4 ">
-      <h2 className="text-left mb-5 text-3xl md:text-5xl uppercase">
-        <div className="font-bold">Simple</div> 
-        <div><span className="font-light text-4xl">But</span><span className="font-bold bg-line ml-3">AMBITIOUS</span></div> 
-      </h2>
-      <div className="border-2 rounded-md  border-slate-700 bg-[#C8D94C]  font-semibold text-2xl p-[30px] md:p-[100px] text-center">
-       {`We are thrilled to announce the launch of our brand-new software house,
-        and we can't wait to partner with you on your next project! At our
-        state-of-the-art development hub, we've assembled a team of highly
-        skilled and experienced senior developers and designers who are eager to
-        bring your ideas to life.`} 
-      </div>
-    </section> */}
-
-      <section className="relative max-w-7xl mx-auto py-[30px] md:py-[50px]">
+      <section className={`${fontPrompt.variable} font-prompt relative max-w-7xl mx-auto py-[30px] md:py-[50px]`}>
       <BackgroundGradientBlur className="top-0 left-0 "/>
       <BackgroundGradientBlur className="bottom-0 right-0"/>
         {/* 1 */}
--- components/Home/TeamCulture/TeamCulture.tsx
+++ components/Home/TeamCulture/TeamCulture.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import BackgroundGradientBlur from "../../backgroundGradientBlur/BackgroundGradientBlur";
+import { Gloria_Hallelujah, Prompt } from "@next/font/google";
+
+const fontPrompt = Prompt({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-prompt",
+});
+
+const fontGloriaHallelujah = Gloria_Hallelujah({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-gloria-hallelujah",
+});
 
 const TeamCulture = () => {
   return (
     <>
-      <section className="relative max-w-7xl mx-auto py-[30px] md:py-[50px]">
+      <section className={`${fontPrompt.variable} font-prompt relative max-w-7xl mx-auto py-[30px] md:py-[50px]`}>
         <BackgroundGradientBlur className="bottom-0 right-0" />
         {/* 1 */}
         <div className="flex justify-between items-center mb-20 z-20">
@@ -15,9 +28,7 @@ const TeamCulture = () => {
               </span>
             </div>
 
-            <ul
-        className="list-none border-2	p-5 rounded-lg min-w-[300px] border-[#A8B738]"
-            >
+            <ul className="list-none border-2	p-5 rounded-lg min-w-[300px] border-[#A8B738]">
               <li>Knowledge sharing</li>
               <li>Flexibility</li>
               <li>Employee empowerment</li>
@@ -30,12 +41,30 @@ const TeamCulture = () => {
           </div>
         </div>
 
-
         {/* 2 */}
-        <div className="text-right text-xl flex flex-col items-center">
-        <div className="w-40 h-1 bg-[#687318] rounded-sm mb-7"></div>
-          <p className="max-w-[600px] text-center">We grant our team the autonomy to work in ways that suit them best, and we create a free and open space for sharing ideas. We believe that when individuals feel empowered and inspired, the magic of creativity flourishes, leading us to greater heights together!
-</p>
+        <div className="text-right text-xl flex flex-col items-center mb-28">
+          <div className="w-40 h-1 bg-[#687318] rounded-sm mb-7"></div>
+          <p className="max-w-[600px] text-center">
+            We grant our team the autonomy to work in ways that suit them best,
+            and we create a free and open space for sharing ideas. We believe
+            that when individuals feel empowered and inspired, the magic of
+            creativity flourishes, leading us to greater heights together!
+          </p>
+        </div>
+
+        {/* 3 */}
+        <div className="text-xl flex flex-col items-center">
+          <div className="md:min-w-[450px]">
+            <div className="font-bold text-4xl  text-left">
+              <span className={`${fontGloriaHallelujah.variable} font-gloriaHallelujah bg-line`}>Good ideas</span>
+            </div>
+            <div className="font-normal text-3xl ml-3 text-center">
+              often come along with
+            </div>
+            <div className="font-bold text-4xl  text-right">
+              <span className={`${fontGloriaHallelujah.variable} font-gloriaHallelujah bg-line`}>FUN!</span>
+            </div>
+          </div>
         </div>
       </section>
     </>
