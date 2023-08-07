f5c9954a17ff4dac30ad8750e0ed9a3463950990
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-07 15:15:44 +0700

    update toggle navbar

--- components/AppLayout/components/NavigationBar/NavigationBar.tsx
+++ components/AppLayout/components/NavigationBar/NavigationBar.tsx
@@ -13,14 +13,14 @@ const NavigationBar = () => {
     
     
   return (
-    <section className="sticky top-0 z-50 bg-[#F9FFCD] lg:bg-inherit	 mb-10  lg:backdrop-blur-[5px]">
-      <nav className="flex flex-col lg:flex-row justify-start lg:justify-between p-7 lg:px-4 lg:py-0 max-w-7xl mx-auto h-screen lg:h-16 items-start lg:items-center ">
+    <section className="sticky top-0 z-50  lg:bg-inherit	 mb-10  lg:backdrop-blur-[5px]">
+      <nav className={`flex flex-col lg:flex-row justify-start lg:justify-between p-7 lg:px-4 lg:py-0 max-w-7xl mx-auto  lg:h-16 items-start lg:items-center  ${isOpen ? 'bg-white' : ''}` }>
         <div className="mb-5 lg:mb-0 relative w-full lg:w-auto">
           <img src="/codework-logo.png" className="h-4 w-auto" alt="xx" />
-           <div className="flex lg:hidden absolute top-0 right-0 text-2xl cursor-pointer" onClick={() => toggleNavbar()}><GiHamburgerMenu/></div> 
+           <div className="flex lg:hidden absolute top-0 right-0 text-2xl cursor-pointer" ><GiHamburgerMenu onClick={() => toggleNavbar()}/></div> 
         </div>
 
-        <div className={`flex flex-col lg:flex-row items-center justify-end gap-x-10 w-full transition-all  ${!isOpen ? '' : ''}`}>
+        <div className={`flex flex-col lg:flex-row items-center justify-end gap-x-10 w-full transition-all duration-1000 ease-out  ${isOpen ? 'flex' : 'hidden'}`}>
           <ul className="grid  grid-cols-1 gap-y-4 lg:gap-y-0 lg:grid-cols-5 justify-start lg:justify-center gap-x-10 justify-items-center items-center text-2xl lg:text-sm text-slate-800 font-bold w-full lg:w-auto mb-10 lg:mb-0">
             <li className="cursor-pointer hover:bg-custom-gradient p-1 w-full border-b-2 border-[#C8D94C] border-dashed lg:border-none">
               Home
@@ -38,7 +38,7 @@ const NavigationBar = () => {
               Clients
             </li>
           </ul>
-          <button className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900  rounded-lg border-2 border-black  bg-[#C8D94C] lg:bg-transparent hover:bg-[#C8D94C]">
+          <button className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900  rounded-lg border-2 border-black   lg:bg-transparent hover:bg-[#C8D94C]">
             CONTACT US
           </button>
         </div>
--- components/AppLayout/components/NavigationBar/withNavigation.tsx
+++ components/AppLayout/components/NavigationBar/withNavigation.tsx
@@ -4,12 +4,6 @@ import React, { useState } from "react";
 const withNavigation = (Component: React.FC) => {
   const Hoc = () => {
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleNavbar = () => {
-      setIsOpen(!isOpen);
-    };
-
     
 
     return <Component  />;
