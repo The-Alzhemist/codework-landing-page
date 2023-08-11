b0e8723349e9b4a895bf27943add4a8a00d9bd47
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-11 11:26:16 +0700

    update

--- components/backgroundGradientBlur/BackgroundGradientBlur.tsx
+++ components/backgroundGradientBlur/BackgroundGradientBlur.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 const BackgroundGradientBlur = ({className}: BgGradientBlueInterface) => {
   return (
     <>
-      {/* <div className={`rounded-full w-[150px] h-[150px] md:w-[500px] md:h-[500px]
-      bg-primary sm:bg-primary-200 blur-[100px] sm:blur-[100px] -z-10 absolute ${className}`}></div> */}
+      <div className={`hidden md:flex rounded-full w-[150px] h-[150px] md:w-[500px] md:h-[500px]
+      bg-primary sm:bg-primary-200 blur-[100px] sm:blur-[100px] -z-10 absolute ${className}`}></div>
     </>
   );
 };
