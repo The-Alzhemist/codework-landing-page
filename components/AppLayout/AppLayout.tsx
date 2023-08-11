1a1b650a178202663fca21d6f8cbc16dbf1d7866
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-11 12:10:00 +0700

    update non bug

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -18,8 +18,12 @@ const AppLayout = ({ children }: FunctionComponentType) => {
     <>
       <NavigationBar font={fontPrompt} />
       {/* <Header/> */}
-      {/* comment for test : bg-primary-100 lg:bg-transparent */}
-      <main className="px-5 text-secondary">{children}</main>
+
+      {/*  bug: */}
+      {/* <main className="px-5 text-secondary">{children}</main> */}
+
+      {/* none bug */}
+      <main className="px-5 text-secondary bg-primary-100 lg:bg-transparent">{children}</main>
       <Footer />
     </>
   );
--- components/backgroundGradientBlur/BackgroundGradientBlur.tsx
+++ components/backgroundGradientBlur/BackgroundGradientBlur.tsx
@@ -5,15 +5,19 @@ import Image from "next/image";
 const BackgroundGradientBlur = ({ className }: BgGradientBlueInterface) => {
   return (
     <>
-      {/* mobile and tablet bg-primary-200 */}
-      {/* <div className={`hidden lg:flex rounded-full w-[150px] h-[150px] md:w-[500px] md:h-[500px]
-    sm:bg-primary-200 blur-[100px] sm:blur-[100px] -z-10 absolute ${className}`}></div> */}
 
-      {/* mobile and tablet bg-primary-200 */}
-      <div
+      {/* bug */}
+      {/* <div
         className={`rounded-full w-[150px] h-[150px] md:w-[500px] md:h-[500px]
     bg-primary-200 blur-[50px] sm:blur-[100px] -z-10 absolute ${className}`}
-      ></div>
+      ></div> */}
+
+
+      {/* none bug */}
+      <div className={`hidden lg:flex rounded-full w-[150px] h-[150px] md:w-[500px] md:h-[500px]
+    sm:bg-primary-200 blur-[100px] sm:blur-[100px] -z-10 absolute ${className}`}></div>
+
+    
     </>
   );
 };
