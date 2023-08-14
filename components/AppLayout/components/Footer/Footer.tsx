7c0131847ad9db6a78239ef24092ee65024711be
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-14 14:15:21 +0700

    add: footer

--- components/AppLayout/components/Footer/Footer.tsx
+++ components/AppLayout/components/Footer/Footer.tsx
@@ -1,28 +1,63 @@
-import React from 'react'
+import { Prompt } from "@next/font/google";
+import React from "react";
+
+const fontPrompt = Prompt({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-prompt",
+});
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900">
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-white sm:text-center ">© 2023 <a href="https://flowbite.com/" className="hover:underline">CodeWork</a>. All Rights Reserved.
-    </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0 text-white">
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">About Us</a>
-        </li>
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Our Services</a>
-        </li>
-        <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">Our Projects</a>
-        </li>
-        <li>
-            <a href="#" className="hover:underline">Our Members</a>
-        </li>
-    </ul>
-    </div>
-</footer>
-  )
-}
+    <footer>
+      <div
+        className={`${fontPrompt.variable} font-prompt relative md:py-[50px] max-w-[1440px] mx-auto p-5 lg:px-[50px] flex flex-col-reverse lg:flex-row gap-y-10 lg:gap-y-0 bg-secondary-1000 text-white`}
+      >
+        {/* left */}
+        <div className="w-full lg:w-1/2">
+          <div>CodeWork Tech Co.,Ltd.</div>
+          <div>3774 Sukhumvit road, South Bang Na, Bang Na, Bangkok 10260</div>
+          <div>Email: hello@codework-tech.com</div>
+          <div className="mb-5"> Tel: (+66) 83 987 4997</div>
+          <div className="text-sm">Ⓒ2023 CodeWork Tech Co.,Ltd. All rights reserved.</div>
+        </div>
 
-export default Footer
+        {/* right */}
+        <div className="w-full lg:w-1/2 flex flex-col gap-y-5">
+          <div className="w-full flex">
+            <div className="w-1/3">
+              <div className="font-semibold">Home</div>
+            </div>
+            <div className="w-1/3">
+              <div className="font-semibold">About us</div>
+              <div>Our story</div>
+              <div>Culture</div>
+              <div>Team</div>
+            </div>
+            <div className="w-1/3">
+              <div className="font-semibold">Services</div>
+
+              <div>Our story</div>
+              <div>Culture</div>
+              <div>Team</div>
+            </div>
+          </div>
+          <div className="w-full flex">
+            <div className="w-1/3">
+              {" "}
+              <div className="font-semibold">Projects</div>
+            </div>
+            <div className="w-1/3">
+              <div className="font-semibold">Products</div>
+            </div>
+            <div className="w-1/3">
+              <div className="font-semibold">Privacy policy </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
