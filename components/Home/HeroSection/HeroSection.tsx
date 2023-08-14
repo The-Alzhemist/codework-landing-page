e85ff3f1d5c01bec21dcbcac9a84efc602e06867
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-14 20:30:55 +0700

    update: herosection responsive

--- components/Home/HeroSection/HeroSection.tsx
+++ components/Home/HeroSection/HeroSection.tsx
@@ -30,8 +30,8 @@ const HeroSection = () => {
       <BackgroundGradientBlur className="bottom-0 right-0" />
 
       <div className="flex w-full">
-        <div className="z-10 w-[100%] lg:w-[80%]  min-h-[600px]  relative bg-white border-[3px] border-black rounded-[50px] py-6 md:p-10 flex flex-col justify-center drop-shadow-md p-2 sm:p-0">
-          <h1 className="mb-20 relative">
+        <div className="z-10 w-[100%] lg:w-[80%]  min-h-[600px]  relative bg-white border-[3px] border-black rounded-[50px] py-6 md:p-10 flex flex-col justify-center drop-shadow-md p-5 sm:p-0">
+          <h1 className="mb-14 md:mb-20 relative">
             <span className="flex text-lg md:text-5xl font-light mb-5 relative">
               <div>WE CAN MAKE YOUR</div> 
               <Image
@@ -45,12 +45,12 @@ const HeroSection = () => {
             </span>
 
             <span
-              className={`${fontGloriaHallelujah.variable} font-gloriaHallelujah text-7xl  `}
+              className={`${fontGloriaHallelujah.variable} font-gloriaHallelujah  text-5xl md:text-7xl `}
             >
               idea come
             </span>
             <span
-              className={`${fontGloriaHallelujah.variable} font-gloriaHallelujah text-7xl  text-primary md:ml-4 flex md:inline relative`}
+              className={`${fontGloriaHallelujah.variable} font-gloriaHallelujah text-5xl md:text-7xl  text-primary md:ml-4 flex md:inline relative mt-5 md:mt-0 `}
             >
               TRUE
               <Image
@@ -58,7 +58,7 @@ const HeroSection = () => {
                 width={0}
                 height={0}
                 layout="responsive"
-                className="hidden md:flex max-w-[200px] max-h-[100px] absolute md:left-[35px] "
+                className="max-w-[150px] sm:max-w-[200px] max-h-[100px] absolute bottom-[-40px] sm:bottom-auto md:left-[35px] "
                 alt="simple but ambitious section cover"
               />
             </span>
