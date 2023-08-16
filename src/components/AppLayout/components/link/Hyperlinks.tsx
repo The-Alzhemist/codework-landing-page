0e2a9dd2ca75c2852737cf8f4b50fee104ff90fd
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-16 10:51:34 +0700

    update code

--- src/components/AppLayout/components/link/Hyperlinks.tsx
+++ src/components/AppLayout/components/link/Hyperlinks.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 import React from 'react'
 import { AiOutlineRight } from "react-icons/ai";
 import { HyperLinkInterfaceProps } from './interface';
+import { twMerge } from 'tailwind-merge';
 
 
 const Hyperlinks = (props:HyperLinkInterfaceProps) => {
+  const style = twMerge(`text-sm font-light hover:border-b-[2px] border-black inline-block }`, props.className)
   return (
-    <Link href={props.link} className={` ${props.className} text-sm font-light hover:border-b-[2px] border-black inline-block }`}>
+    <Link href={props.link} className={style}>
       <div className="flex items-center"> {props.children}  <span className="ml-1"><AiOutlineRight/></span></div>
     </Link>
   )
