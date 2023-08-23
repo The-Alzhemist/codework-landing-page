48296ef405f88fcf5a58678a1294e92f1bb52ec7
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-23 15:42:30 +0700

    update: type for formspree-form.tsx

--- src/components/ContactUs/withFormSpree-form.tsx
+++ src/components/ContactUs/withFormSpree-form.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { FormValues, WithFormSpreeFormProps } from "./interface";
+import { useForm } from "react-hook-form";
 
-const withFormSpreeForm = (Component: React.FC) => {
-   
+const withFormSpreeForm = (Component: React.FC<WithFormSpreeFormProps>) => {
   const Hoc = () => {
-    const newProps:any = {
-    
+    const [isShowOtherChannel, setIsShowOtherChannel ] = useState<boolean>(false);
+    const hookForm = useForm<FormValues>();
+  const { register, handleSubmit, formState } = hookForm;
+  const { errors, isValid } = formState;
+    const newProps:WithFormSpreeFormProps = {
+      isShowOtherChannel,
+      setIsShowOtherChannel,
+      register,
+      handleSubmit,
+      errors,
+      isValid
     };
     return <Component {...newProps} />;
   };
