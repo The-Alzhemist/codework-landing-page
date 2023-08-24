2dc57862808437ab580de9029989e9a6a9de1ece
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-24 10:39:56 +0700

    update: toastlify

--- src/components/ContactUs/component/statusMessage/StatusMessage.tsx
+++ src/components/ContactUs/component/statusMessage/StatusMessage.tsx
@@ -1,38 +1,55 @@
-import React from 'react'
-import { FormSpreeStatus } from './interface'
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect } from 'react';
+import { FormSpreeStatus } from './interface';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const StatusMessage = (state:FormSpreeStatus) => {
-    if(state.submitting) {
-         toast.info("Sending your message, please wait...", {
-          position: "top-center",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        })
-
-     
-    } else if (state.succeeded ) {
-      toast.success("Your message has been sent successfully. Thank you for reaching out to us!", {
+const StatusMessage = ({ succeeded, submitting, errors }: FormSpreeStatus) => {
+  useEffect(() => {
+    if (submitting && !succeeded) {
+      toast.info("We're processing your message...", {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
         theme: "light",
-      })
-    } else if (state.errors) {
-       return(<div className="text-red-500 mt-3 px-2 md:px-5">
-       {`Sorry, we couldn't send your message. Please try again`}
-     </div>) 
+      });
+    } else {
+      toast.dismiss();
     }
-}
+
+    if (!submitting && succeeded) {
+      toast.success("Message sent successfully!", {
+        position: "top-center",
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    } else {
+      toast.dismiss();
+    }
+    if (errors) { 
+      toast.error("There was an issue sending your message. Please retry", {
+        position: "top-center",
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+
+  }, [succeeded, submitting, errors]);
+
+  return null
+};
 
 export default StatusMessage
