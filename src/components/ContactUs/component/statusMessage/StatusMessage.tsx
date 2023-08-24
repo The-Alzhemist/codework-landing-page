3bb078de78f879f6d6c8f15d84045527de9957af
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-24 08:04:15 +0700

    add: react-toastlify

--- src/components/ContactUs/component/statusMessage/StatusMessage.tsx
+++ src/components/ContactUs/component/statusMessage/StatusMessage.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import { FormSpreeStatus } from './interface'
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const StatusMessage = (state:FormSpreeStatus) => {
     if(state.submitting) {
-        return (<div className="text-secondary-900 mt-3 px-2 md:px-5">
-        {`Sending your message, please wait...`} 
-      </div>)
+         toast.info("Sending your message, please wait...", {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        })
+
+     
     } else if (state.succeeded ) {
-        return (<div className="text-primary-800 mt-3 px-2 md:px-5">
-        {`Your message has been sent successfully. Thank you for reaching out
-        to us!`}
-      </div>)
+      toast.success("Your message has been sent successfully. Thank you for reaching out to us!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      })
     } else if (state.errors) {
        return(<div className="text-red-500 mt-3 px-2 md:px-5">
        {`Sorry, we couldn't send your message. Please try again`}
