489a5a1e8da34ba8baa3c7bbac9d80d761ed746b
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-24 16:23:17 +0700

    add: reset function

--- src/components/ContactUs/withFormSpree-form.tsx
+++ src/components/ContactUs/withFormSpree-form.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ContactFormTypes, WithFormSpreeFormProps } from "./interface";
 import { FieldValues, useForm } from "react-hook-form";
 import { useForm as useFormSpree } from "@formspree/react";
-import { PRIVATE_MAIL_TEST, FORMSPREE_LANDING_TEST_KEY } from "@/config/environment";
+import { PRIVATE_MAIL_TEST, FORMSPREE_LANDING_TEST_KEY, FORMSPREE_LANDING_KEY } from "@/config/environment";
 
 const withFormSpreeForm = (Component: React.FC<WithFormSpreeFormProps>) => {
   const Hoc = () => {
     const [isShowOtherChannel, setIsShowOtherChannel] =
       useState<boolean>(false);
     const hookForm = useForm<FieldValues>();
-    const { register, handleSubmit, formState } = hookForm;
+    const { register, handleSubmit, formState, reset } = hookForm;
     const { errors, isValid } = formState;
-    const [state, sendDataToFromSpree] = useFormSpree(PRIVATE_MAIL_TEST);
+    const [state, sendDataToFromSpree] = useFormSpree(FORMSPREE_LANDING_KEY);
+
+    useEffect(() => {
+      if (!state.submitting && !state.errors && state.succeeded) {
+        console.log('state >>>>', state)
+        reset(); 
+      }
+    }, [state.submitting, state.errors, state.succeeded]);
+    
     const newProps: WithFormSpreeFormProps = {
       isShowOtherChannel,
       setIsShowOtherChannel,
@@ -22,6 +30,8 @@ const withFormSpreeForm = (Component: React.FC<WithFormSpreeFormProps>) => {
       state,
       sendDataToFromSpree
     };
+
+    
     return <Component {...newProps} />;
   };
 
