b809cc4208edacb6b7f79de07e0d7ec21188a859
Aphiwit1 <aphiwit.boon@gmail.com>
2023-08-31 16:25:14 +0700

    add: gtm-mock

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import  TagManager  from 'react-gtm-module';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5BSHT4MJ' });
+  }, []);
+  
   return <Component {...pageProps} />
 }
