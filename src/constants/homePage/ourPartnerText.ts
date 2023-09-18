a1b62346fb25e2cebdbf34c884582ea378e163c6
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-09-18 11:38:21 +0700

    update img

--- src/constants/homePage/ourPartnerText.ts
+++ src/constants/homePage/ourPartnerText.ts
@@ -6,7 +6,7 @@ export const ourPartnerSectionText: ourPartnerTextInterface = {
     heading2: "PARTNERS",
     imagesList: [
       {pathName: '/semour.png', alt:'semour logo', urlName: 'https://seamoormarine.com'},
-      {pathName: '/home/our-partner/ks_seaport.png', alt:'KS seaport logo', urlName: 'https://wwww.facebook.com/p/KS-Sea-Port-Company-Limited-100054525187944'},
+      {pathName: '/home/our-partner/KS_seaport.png', alt:'KS seaport logo', urlName: 'https://wwww.facebook.com/p/KS-Sea-Port-Company-Limited-100054525187944'},
       {pathName: '/home/our-partner/tonsai_market.png', alt:'Tonsai market logo', urlName: 'https://www.facebook.com/Tonsaimarket456'},
     ]
   }
