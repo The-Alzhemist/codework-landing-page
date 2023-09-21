4ae4dc1bfd19b19cec2b4bd03108d1b5b3a9cb07
AphiwitCodeWork <139746122+AphiwitCodeWork@users.noreply.github.com>
2023-09-21 16:00:36 +0700

    update: animation

--- src/components/AppLayout/components/NavigationBar/NavigationBar.tsx
+++ src/components/AppLayout/components/NavigationBar/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import withNavigation from "./withNavigation";
 import Link from "next/link";
 import { animated } from "@react-spring/web";
@@ -25,16 +25,20 @@ const NavigationBar = ({
   };
 
   const controls = useAnimation();
+  
+  useEffect(() => {
+      controls.start({ y: "-100%" }); // Move sidebar up (Y: -100%)
+  }, []);
 
-  const animateSidebar = async () => {
+
+  useEffect(() => {
     if (isOpen) {
-      await controls.start({ y: 0 }); // Move sidebar down (Y: 0%)
+      controls.start({ y: 0 }); // Move sidebar down (Y: 0%)
     } else {
-      await controls.start({ y: '-100%' }); // Move sidebar up (Y: -100%)
+      controls.start({ y: "-100%" }); // Move sidebar up (Y: -100%)
     }
-  };
+  }, [isOpen]);
 
- 
   return (
     <>
       <header
@@ -64,7 +68,6 @@ const NavigationBar = ({
               className="cursor-pointer p-3 bg-slate-300 rounded"
               onClick={() => {
                 toggleSidebar();
-                animateSidebar();
               }}
             >
               menu
@@ -77,63 +80,64 @@ const NavigationBar = ({
       </header>
 
       <AnimatePresence>
-          <motion.nav
-            animate={controls}
-            className={`top-0 h-full w-full z-50 $ fixed`}
-          >
-            <header className="bg-primary-500 h-auto p-10 grid grid-cols-2">
-              <div
-                className="flex justify-end font-semibold col-start-1 col-span-2"
-                onClick={() => {
-                  toggleSidebar();
-                  animateSidebar();
-                }}
-              >
-                <span className="bg-slate-50 p-3 rounded-full cursor-pointer">
-                  X
+        <motion.nav
+          animate={controls}
+          className={`top-0 h-full w-full z-50 fixed`}
+        >
+          <header className="bg-primary-500 h-auto p-10 grid grid-cols-2">
+            <div
+              className="flex justify-end font-semibold col-start-1 col-span-2"
+              onClick={() => {
+                toggleSidebar();
+              }}
+            >
+              <span className="bg-slate-50 w-12 h-12 p-3 rounded-full cursor-pointer flex justify-center items-center">
+                X
+              </span>
+            </div>
+            <motion.div className="flex flex-col gap-y-3">
+              <div className=" cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
+                Home
+                <span className="hidden group-hover:block">
+                  <FaArrowLeftLong />
                 </span>
               </div>
-              <div className="flex flex-col gap-y-3">
-                <div className=" cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
-                  Home
-                  <span className="hidden group-hover:block">
-                    <FaArrowLeftLong />
-                  </span>
-                </div>
-                <div className="cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
-                  About Us
-                  <span className="hidden group-hover:block">
-                    <FaArrowLeftLong />
-                  </span>
-                </div>
-                <div className="cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
-                  Our Product
-                  <span className="hidden group-hover:block">
-                    <FaArrowLeftLong />
-                  </span>
-                </div>
-                <div className="cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
-                  Contact Us
-                  <span className="hidden group-hover:block">
-                    <FaArrowLeftLong />
-                  </span>
-                </div>
+              <div className="cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
+                About Us
+                <span className="hidden group-hover:block">
+                  <FaArrowLeftLong />
+                </span>
               </div>
-              <div className="flex flex-col items-end justify-end">
-                <div className="text-lg">Social</div>
-                <div className="text-2xl font-semibold">Facebook</div>
-                <div className="text-2xl font-semibold mb-5">Instragram</div>
-
-                <div className="text-lg">Phone</div>
-                <div className="text-2xl font-semibold mb-5">(+66) 83 987 4997</div>
-
-                <div className="text-lg">Email</div>
-                <div className="text-2xl font-semibold">hello@codework-tech.com</div>
+              <div className="cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
+                Our Product
+                <span className="hidden group-hover:block">
+                  <FaArrowLeftLong />
+                </span>
               </div>
-            </header>
-          </motion.nav>
-        
-        ◊
+              <div className="cursor-pointer text-7xl font-bold flex gap-x-2 hover:text-secondary group">
+                Contact Us
+                <span className="hidden group-hover:block">
+                  <FaArrowLeftLong />
+                </span>
+              </div>
+            </motion.div>
+            <div className="flex flex-col items-end justify-end">
+              <div className="text-lg">Social</div>
+              <div className="text-2xl font-semibold">Facebook</div>
+              <div className="text-2xl font-semibold mb-5">Instagram</div>
+
+              <div className="text-lg">Phone</div>
+              <div className="text-2xl font-semibold mb-5">
+                (+66) 83 987 4997
+              </div>
+
+              <div className="text-lg">Email</div>
+              <div className="text-2xl font-semibold">
+                hello@codework-tech.com
+              </div>
+            </div>
+          </header>
+        </motion.nav>
       </AnimatePresence>
 
       {/* Margin */}
